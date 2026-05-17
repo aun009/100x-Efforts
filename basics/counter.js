3ad630fe224ba9aec07edc867d09bea986a8b502
aun009 <arunmahajan9240@gmail.com>
2026-05-17 22:12:11 +0530

    notes fullstack app with jwt

---
+++ basics/counter.js
@@ -0,0 +1,21 @@
+
+let cnt = 0;
+
+function useCallback() {
+    console.log(cnt);
+    cnt++;
+    
+}
+
+setInterval(useCallback, 1000);
+
+
+console.log("Arun");
+
+let sum = 0;
+for(let i=0; i<10000000000; i++) {
+    sum += i;
+    
+}
+
+console.log(sum);
